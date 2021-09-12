11db2cf5450e88da141872fa0e555855b4ba8656
mkw18 <56500720+mkw18@users.noreply.github.com>
2021-09-13 01:18:13 +0800

    first commit

---
+++ decoder.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import numpy as np
+from rlpyt.utils.tensor import infer_leading_dims, restore_leading_dims
+from kornia.augmentation import RandomAffine,\
+    RandomCrop,\
+    CenterCrop, \
+    RandomResizedCrop
+from kornia.filters import GaussianBlur2d
+import copy
+from models import Conv2dModel
+
+class SPRDecodeModel(nn.Module):
+    def __init__(self, jumps, augmentation, imagesize = 256, final_classifier = "linear"):
+        super().__init__()
+        self.classifier_type = "mlp"
+        self.jumps = jumps
+        self.time_offset = 0
+        self.momentum_tau = 0.01
+        self.renormalize = True
+        self.target_augmentation = 1,
+        self.eval_augmentation = 0,
+        
+        self.transforms = []
+        self.eval_transforms = []
+
+        self.uses_augmentation = False
+        for aug in augmentation:
+            if aug == "affine":
+                transformation = RandomAffine(5, (.14, .14), (.9, 1.1), (-5, 5))
+                eval_transformation = nn.Identity()
+                self.uses_augmentation = True
+            elif aug == "crop":
+                transformation = RandomCrop((84, 84))
+                # Crashes if aug-prob not 1: use CenterCrop((84, 84)) or Resize((84, 84)) in that case.
+                eval_transformation = CenterCrop((84, 84))
+                self.uses_augmentation = True
+                imagesize = 84
+            elif aug == "rrc":
+                transformation = RandomResizedCrop((100, 100), (0.8, 1))
+                eval_transformation = nn.Identity()
+                self.uses_augmentation = True
+            elif aug == "blur":
+                transformation = GaussianBlur2d((5, 5), (1.5, 1.5))
+                eval_transformation = nn.Identity()
+                self.uses_augmentation = True
+            elif aug == "shift":
+                transformation = nn.Sequential(nn.ReplicationPad2d(4), RandomCrop((84, 84)))
+                eval_transformation = nn.Identity()
+            elif aug == "intensity":
+                transformation = Intensity(scale=0.05)
+                eval_transformation = nn.Identity()
+            elif aug == "none":
+                transformation = eval_transformation = nn.Identity()
+            else:
+                raise NotImplementedError()
+            self.transforms.append(transformation)
+            self.eval_transforms.append(eval_transformation)
+            
+        self.conv = Conv2dModel(
+            in_channels=3,
+            channels=[32, 64, 64],
+            kernel_sizes=[8, 4, 3],
+            strides=[4, 2, 1],
+            paddings=[0, 0, 0],
+            use_maxpool=False,
+            dropout=0,
+        )
+        
+        self.decode = RevConv2dModel(
+            in_channels=64,
+            channels=[64, 32, 3],
+            kernel_sizes=[3, 4, 8],
+            strides=[1, 2, 4],
+            paddings=[0, 0, 0],
+            use_maxpool=False,
+            dropout=0,
+        )
+        
+        
+        
+class RevConv2dModel(torch.nn.Module):
+    """2-D Convolutional model component, with option for max-pooling vs
+    downsampling for strides > 1.  Requires number of input channels, but
+    not input shape.  Uses ``torch.nn.Conv2d``.
+    """
+
+    def __init__(
+            self,
+            in_channels,
+            channels,
+            kernel_sizes,
+            strides,
+            paddings=None,
+            nonlinearity=torch.nn.ReLU,  # Module, not Functional.
+            use_maxpool=False,  # if True: convs use stride 1, maxpool downsample.
+            head_sizes=None,  # Put an MLP head on top.
+            dropout=0.,
+            ):
+        super().__init__()
+        if paddings is None:
+            paddings = [0 for _ in range(len(channels))]
+        assert len(channels) == len(kernel_sizes) == len(strides) == len(paddings)
+        in_channels = [in_channels] + channels[:-1]
+        ones = [1 for _ in range(len(strides))]
+        if use_maxpool:
+            maxp_strides = strides
+            strides = ones
+        else:
+            maxp_strides = ones
+        conv_layers = [nn.ConvTranspose2d(in_channels=ic, out_channels=oc,
+            kernel_size=k, stride=s, padding=p) for (ic, oc, k, s, p) in
+            zip(in_channels, channels, kernel_sizes, strides, paddings)]
+        sequence = list()
+        for conv_layer, maxp_stride in zip(conv_layers, maxp_strides):
+            sequence.extend([conv_layer, nonlinearity()])
+            if dropout > 0:
+                sequence.append(nn.Dropout(dropout))
+            if maxp_stride > 1:
+                sequence.append(torch.nn.MaxPool2d(maxp_stride))  # No padding.
+        self.conv = torch.nn.Sequential(*sequence)
+
+    def forward(self, input):
+        """Computes the convolution stack on the input; assumes correct shape
+        already: [B,C,H,W]."""
+        return self.conv(input)
